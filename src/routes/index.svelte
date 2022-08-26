5ada177564737d10cd607c8473bc2da3a6f751ad
lums <jeremy@lums.io>
2022-08-26 21:36:25 +0200

    Main content style tweaks

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,13 +1,25 @@
 <script lang="ts">
-    import Scene from '$lib/BabylonMain.svelte';
-    import SideMenu from '$lib/SideMenu.svelte';
+	import Scene from '$lib/BabylonMain.svelte';
+	import SideMenu from '$lib/SideMenu.svelte';
 </script>
 
 <div>
-    hello
-    <p>heres some content. try to load babylonjs from here</p>
+	<h1>hello</h1>
+	<p>heres some content. try to load babylonjs from here</p>
 </div>
 
-<SideMenu/>
+<Scene />
+<SideMenu />
 
-<Scene/>
+<style>
+	div {
+		font-size: 1.2rem;
+		position: fixed;
+		z-index: 15;
+		color: #eee;
+		padding: 1rem;
+		margin: 0.5rem 0.8rem;
+		border-radius: 0.3rem;
+		filter: drop-shadow(4px 2px 1px #0f0f0f);
+	}
+</style>
