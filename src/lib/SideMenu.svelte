58d133c83b73cbb12b8a138bf1cbaa17a80b65dc
lums <jeremy@lums.io>
2022-08-26 21:35:48 +0200

    Minor style tweaks

--- src/lib/SideMenu.svelte
+++ src/lib/SideMenu.svelte
@@ -1,25 +1,25 @@
 <script lang="ts">
-	import {my_store, slider} from '../store';
+	import { my_store, slider } from '../store';
 </script>
 
-
 <div>
-	
 	<label>
-		<input type=checkbox bind:checked={$my_store}>
+		<input type="checkbox" bind:checked={$my_store} />
 		toggle extra object
-		<input type="range" bind:value="{$slider}" name="" id="" min="-1" max="1" step="0.01">
+		<input type="range" bind:value={$slider} name="" id="" min="-1" max="1" step="0.01" />
 	</label>
-	<button>menu item 2</button>
 	<span>{$slider}</span>
 </div>
 
 <style>
-	div{
-		position: fixed;
+	div {
+		position: relative;
+		top: 2rem;
 		z-index: 10;
 		background-color: #232323;
-		color: #EEE;
+		color: #eee;
 		padding: 1rem;
+		margin: 0.5rem 0.8rem;
+		border-radius: 0.3rem;
 	}
 </style>
