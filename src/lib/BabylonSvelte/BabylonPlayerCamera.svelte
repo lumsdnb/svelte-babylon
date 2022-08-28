3e4ffcae3036f8b170c9f35e9d9837fb07ff9b20
lums <jeremy@lums.io>
2022-08-28 20:15:02 +0200

    init

---
+++ src/lib/BabylonSvelte/BabylonPlayerCamera.svelte
@@ -0,0 +1,46 @@
+<script lang="ts">
+	import type { ArcRotateCamera, Vector3 } from 'babylonjs';
+
+	import { getContext, onMount, onDestroy } from 'svelte';
+
+	const { getScene } = getContext('BabylonScene');
+	const scene = getScene();
+
+	let camera: ArcRotateCamera;
+
+	export let name: string = null;
+	$: if (name && camera) camera.name = name;
+
+	export let position: Vector3 = null;
+	$: if (position && camera) camera.position = position;
+
+	export let target: Vector3 = null;
+	$: if (target && camera) camera.setTarget(target);
+
+	let BABYLON: typeof import('babylonjs');
+
+	$: if (BABYLON && $scene && !camera) {
+		if (!name) name = '';
+		if (!position) position = new BABYLON.Vector3(0, 0, 0);
+		if (!target) target = new BABYLON.Vector3(0, 0, 0);
+
+		camera = new BABYLON.ArcRotateCamera("Camera", 3 * Math.PI / 2, -Math.PI / 2, 50, BABYLON.Vector3.Zero(), $scene);
+
+		// camera.applyGravity = true;
+		camera.useFramingBehavior = true;
+
+		const canvas = $scene.getEngine().getRenderingCanvas();
+		camera.attachControl(canvas, true);
+	}
+
+	onMount(async () => {
+		const babylonjs = await import('babylonjs');
+		BABYLON = babylonjs.default;
+	});
+
+	onDestroy(() => {
+		if ($scene && camera) $scene.removeCamera(camera);
+	});
+</script>
+
+<slot />
--- src/lib/BabylonSvelte/index.ts
+++ src/lib/BabylonSvelte/index.ts
@@ -1,6 +1,7 @@
 export { default as BabylonEngine } from './BabylonEngine.svelte';
 export { default as BabylonScene } from './BabylonScene.svelte';
 export { default as BabylonCamera } from './BabylonCamera.svelte';
+export { default as BabylonPlayerCamera } from './BabylonPlayerCamera.svelte';
 export { default as BabylonHemisphericLight } from './BabylonHemisphericLight.svelte';
 export { default as BabylonSphere } from './BabylonSphere.svelte';
 export { default as BabylonBox } from './BabylonBox.svelte';
